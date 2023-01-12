f2cd7ef827ac4a04c223d52c4c610dadab5cce87
musi <liuziming.lzm@alibaba-inc.com>
2023-01-12 10:17:01 +0800

    feat: add example shell script

---
+++ spring-cloud-alibaba-examples/governance-example/label-routing-example/default-provider-version-example/bin/check_broker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [ ! -z $DEBUG_MODE ]; then
+  echo "OK"
+  exit 0
+fi
+
+
+PORT=80
+if [ $SERVER_PORT ] ; then
+  PORT=$SERVER_PORT
+fi
+
+ret=`curl http://127.0.0.1:$PORT/checkpreload`
+if [ "$ret" = "success" ];then
+	echo "OK"
+	exit 0
+else
+  ret=`curl http://127.0.0.1:8080/checkpreload`
+  if [ "$ret" = "success" ];then
+  	echo "OK"
+  	exit 0
+  else
+    echo "ERROR"
+  	exit 1
+  fi
+fi
---
+++ spring-cloud-alibaba-examples/governance-example/label-routing-example/istio-consumer-example/aliyun_build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# mvn打包项目
+mvn clean package -DskipTests
+
+repository=registry.cn-shenzhen.aliyuncs.com
+aliyun_user=csb_dev_test@test.aliyunid.com
+aliyun_password=sca@1234
+namespace=sca-demo
+image=istio-consumer-example
+#version=`git branch | sed -n '/\* /s///p'`
+version=develop
+
+docker buildx inspect --bootstrap
+docker buildx build -t $repository/$namespace/$image:$version --platform linux/amd64 -o type=docker .
+
+docker login $repository --username $aliyun_user --password $aliyun_password
+docker push $repository/$namespace/$image:$version
